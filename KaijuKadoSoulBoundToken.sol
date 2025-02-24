00d093a06d0fc58b3e7288428622e7704d186bf9
mattkaiju <matt@kaiju.fi>
2025-02-24 20:37:46 +0800

    Update KaijuKadoSoulBoundToken.sol

--- KaijuKadoSoulBoundToken.sol
+++ KaijuKadoSoulBoundToken.sol
@@ -1,1 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract KaijuKadoSoulBoundToken is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+    string private _imageUri;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory imageUri) public initializer {
+        __ERC721_init("KaijuKadoSoulBoundToken", "KKSBT");
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        _imageUri = imageUri;
+    }
+
+    function safeMint(address to) public onlyOwner {
+        require(balanceOf(to) == 0, 'User already owns a soulbound token');
+         
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override
+        returns (address)
+    {
+        address from = _ownerOf(tokenId);
+        require(from == address(0) || to == address(0), "This a Soulbound token. It cannot be transferred. It can only be burned by the token owner.");
+        return super._update(to, tokenId, auth);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, _imageUri) : "";
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
